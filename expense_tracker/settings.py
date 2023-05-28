2430979d48788d4e9d7393138bd5b30d5ad6ba18
Akshatha.L <akshatha.26997@gmail.com>
2023-05-28 16:28:11 +0530

    render changes

--- expense_tracker/settings.py
+++ expense_tracker/settings.py
@@ -79,12 +79,12 @@ WSGI_APPLICATION = 'expense_tracker.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
